190be7c5fac829c64336a138f80193ab5dbc6a6f
fatima <oud.tima@hotmail.com>
2021-10-25 03:26:35 +0200

    price

--- front/js/cart.js
+++ front/js/cart.js
@@ -4,7 +4,9 @@ let cart = JSON.parse(localStorage.cart);
 const cartItems = document.getElementById("cart__items");
 // console.log(cartItems);
 const totalQuantity = document.getElementById("totalQuantity");
-// console.log(totalQuantity);
+console.log(totalQuantity);
+const totalPrice = document.getElementById("totalPrice");
+console.log(totalPrice);
 
 cartItems.innerHTML = cart
   .map(
@@ -16,7 +18,7 @@ cartItems.innerHTML = cart
     <div class="cart__item__content">
     <div class="cart__item__content__titlePrice">
     <h2>${product.name}</h2>
-    <p>${product.price} €</p>
+    <p>${product.price * product.quantity} €</p>
     </div>
     <div class="cart__item__content__settings">
     <div class="item__content__settings__color">
@@ -24,10 +26,14 @@ cartItems.innerHTML = cart
     </div>
     <div class="cart__item__content__settings__quantity">
     <p>Qté : </p>
-    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${product.quantity} onchange="changeQuantity(event,'${product._id}')">
+    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${
+      product.quantity
+    } onchange="changeQuantity(event,'${product._id}')">
     </div>
     <div class="cart__item__content__settings__delete">
-    <p class="deleteItem" onClick="deletProduct('${product._id}','${product.selectedColor}');">Supprimer</p>
+    <p class="deleteItem" onClick="deletProduct('${product._id}','${
+      product.selectedColor
+    }');">Supprimer</p>
     </div>
     </div>
     </div>
@@ -37,19 +43,35 @@ cartItems.innerHTML = cart
 
 const deletProduct = (productId, productColor) => {
   //Avec la méthode filter je sélectionne les éléments à garder et je supprime l'élément où le btn suppr a été cliqué
-
-  cart = cart.filter((item) => item._id != productId && item.selectedColor != productColor);
+  cart = cart.filter(
+    (item) => item._id != productId && item.selectedColor != productColor
+  );
   localStorage.setItem("cart", JSON.stringify(cart));
   console.log(cart);
+  location.reload()
 };
+
+// for (i of cart) {
+//     console.log(i.price);
+//     console.log(i.quantity);
+//     i.price = i.price*i.quantity;
+//     console.log(i.price*i.quantity);
+//     console.log(cart);
+//   }
 const changeQuantity = (e, productId) => {
-    console.log(e.target.value);
-    for(i of cart) {
-        // console.log(i._id);
-        // console.log(productId);
-        if (i._id === productId){ 
-            i.quantity= e.target.value;
-        }
+  console.log(e.target.value);
+  for (i of cart) {
+    // console.log(i._id);
+    // console.log(productId);
+    if (i._id === productId) {
+      i.quantity = e.target.value;
     }
-    localStorage.setItem("cart", JSON.stringify(cart));
-}
+    // totalPrice += i.price * i.quantity;
+    // console.log(i.price * i.quantity);
+  }
+  
+  localStorage.setItem("cart", JSON.stringify(cart));
+  location.reload()
+};
+
+
